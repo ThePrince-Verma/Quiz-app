a3a6d3cd8102c70a55c3218e506b1e291d8a55ee
ThePrince-Verma <prince.pv759@gmail.com>
2024-09-26 18:57:58 +0530

    first commit

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+  useEffect(() => {
+    console.log("timeout");
+    const timer = setTimeout(onTimeout, timeout);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onTimeout, timeout]);
+
+  useEffect(() => {
+    console.log("setting interval");
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+    return () => {
+      clearInterval(interval); //cleanup function
+    };
+  }, []);
+
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
+}
